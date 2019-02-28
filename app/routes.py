1d28d5ebae5bab6d7be9b49d67d055d4e425d689
Artem <artem.luceno@gmail.com>
2019-02-28 20:22:58 +0200

    first_commit

---
+++ app/routes.py
@@ -0,0 +1,52 @@
+from app import app
+from flask import render_template
+
+news1 = {
+    'id': 1,
+    'title': 'Новость1',
+    'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur dolor enim esse '
+                   'laboriosam, nam numquam odio, officia perspiciatis quaerat, rerum soluta tenetur veritatis '
+                   'voluptatibus. Assumenda iure maxime minus optio placeat!',
+    'img': '/static/img/news3.jpg',
+    'text': '................',
+    'author': 'Новикова А.Н.',
+    'date': '12-01-2019'
+}
+news2 = {
+    'id': 2,
+    'title': 'Новость2',
+    'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur dolor enim esse '
+                   'laboriosam, nam numquam odio, officia perspiciatis quaerat, rerum soluta tenetur veritatis '
+                   'voluptatibus. Assumenda iure maxime minus optio placeat!',
+    'img': '/static/img/news1.jpg',
+    'text': '................',
+    'author': 'Новикова А.Н.',
+    'date': '12-01-2019'
+}
+news3 = {
+    'id': 3,
+    'title': 'Новость3',
+    'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur dolor enim esse '
+                   'laboriosam, nam numquam odio, officia perspiciatis quaerat, rerum soluta tenetur veritatis '
+                   'voluptatibus. Assumenda iure maxime minus optio placeat!',
+    'img': '/static/img/news2.jpg',
+    'text': '................',
+    'author': 'Новикова А.Н.',
+    'date': '12-01-2019'
+}
+news_list = [news1, news2, news3]
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template("index.html", news_list=news_list)
+
+
+@app.route('/news')
+def news():
+    return render_template('news.html', news_list=news_list)
+
+@app.route("/news/<int:id>")
+def single(id):
+    return render_template('single.html', news=news1)
